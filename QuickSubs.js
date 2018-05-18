bbd9e1d5ea4242695fa2989af8fe957385d25b56
Eric Wescott <wescotte@gmail.com>
2018-05-18 06:46:21 -0500

    1.03 Release
    Fixed some CSS/layout issues.

--- QuickSubs.js
+++ QuickSubs.js
@@ -3,15 +3,15 @@ Copyright 2014 Eric Wescott - wescotte@gmail.com
 
 This file is part of Quick Subtitles.
 
-Foobar is free software: you can redistribute it and/or modify it under the terms of the
+Quick Subtitles is free software: you can redistribute it and/or modify it under the terms of the
 GNU General Public License as published by the Free Software Foundation, either version 3
 of the License, or (at your option) any later version.
 
-Foobar is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+Quick Subtitles is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
 without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 See the GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License along with Foobar. If
+You should have received a copy of the GNU General Public License along with Quick Subtitles. If
 not, see http://www.gnu.org/licenses/.
 */
 
